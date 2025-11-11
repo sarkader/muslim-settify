b3e5d0f5c83120b98e5f2d4221e69f72f5fa64fe
sarkader <sairkdr@gmail.com>
2025-11-11 15:50:28 +0300

    chore: update favicon to settify logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Muslim Settify - Earn $3k-$15k Monthly with Remote Appointment Setting",
   description: "Professional training program helping Muslims master remote appointment setting. Build a profitable career from home with our proven system.",
+  icons: {
+    icon: "/settify-bg.png",
+  },
 };
 
 export default function RootLayout({
